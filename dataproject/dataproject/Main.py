3a9edb61b62e7382d024fc3365eddb71b772efa2
august <august@moos-bjerre.dk>
2019-03-18 22:38:17 +0100

    importet data from OECD, does not work yet

--- dataproject/dataproject/Main.py
+++ dataproject/dataproject/Main.py
@@ -4,3 +4,16 @@
 #to upload changes: 
 #1) git commit 
 #2) git sync
+
+import pip 
+import pandas as pd
+#if you don't have pandas_datareader installed write: "pip install pandas_datareader" without quotes in the terminal
+import pandas_datareader as pdr
+import numpy as np
+
+
+#import of data
+#Here we import the CO2 database from OECD
+df = pdr.DataReader("AIR_GHG","oecd")
+type(df)
+#something has gone wrong here. the data is here but it is a mess
