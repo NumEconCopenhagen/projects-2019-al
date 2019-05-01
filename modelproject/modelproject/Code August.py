f940d805028c5fd37c8190c14557cc3886ee1657
August <august@MBLAs-MacBook-Pro.local>
2019-05-01 18:15:56 +0200

    August file

---
+++ modelproject/modelproject/Code August.py
@@ -0,0 +1,24 @@
+#cournot model
+
+import numpy as np
+import scipy as sp
+import sympy as sm
+
+sm.init_printing(use_unicode=True)
+
+q1 = sm.symbols('q_1')
+q2 = sm.symbols('q_2')
+c1 = sm.symbols('c_1')
+c2 = sm.symbols('c_2')
+a = sm.symbols('a')
+pi1 = sm.symbols('pi_1')
+pi2 = sm.symbols('pi_2')
+p = sm.symbols('p')
+p1 = (a-(q1+q2)*q1)
+p2 = (a-(q1+q2)*q2)
+pi1 =(p1*q1-c1*q1)
+pi2 = (p2*q2-c2*q2)
+
+
+
+
