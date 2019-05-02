253c34561d792091c16d8a3f654ad74a610d49c8
August <august@MBLAs-MacBook-Pro.local>
2019-05-02 12:49:25 +0200

    adsfgh

--- modelproject/modelproject/Code August.py
+++ modelproject/modelproject/Code August.py
@@ -5,7 +5,7 @@ import scipy as sp
 import sympy as sm
 
 sm.init_printing(use_unicode=True)
-
+#defining all variables as symby symbols 
 q1 = sm.symbols('q_1')
 q2 = sm.symbols('q_2')
 c1 = sm.symbols('c_1')
@@ -14,10 +14,14 @@ a = sm.symbols('a')
 pi1 = sm.symbols('pi_1')
 pi2 = sm.symbols('pi_2')
 p = sm.symbols('p')
+
+#Defining relations between symbols
 p1 = (a-(q1+q2)*q1)
 p2 = (a-(q1+q2)*q2)
 pi1 =(p1*q1-c1*q1)
 pi2 = (p2*q2-c2*q2)
+
+
 
 
 
